a23953102431072a6764d106845dc5d31d152d77
Miras-SZS <73289669+Miras-SZS@users.noreply.github.com>
2020-10-22 18:09:48 +0600

    e-shop
    test

---
+++ config/db_params.php
@@ -0,0 +1,9 @@
+<?php
+
+// Заполните поля для Вашей БД
+return array(
+    'host' => 'zinchenko',
+    'dbname' => 'phpshop',
+    'user' => 'wpfolder',
+    'password' => 'kaskadrom',            
+);
